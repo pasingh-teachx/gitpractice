3bd55ce390f4866503340f9e707dc66118a3bc3d
Paritosh Singh <pasingh@teachx.ai>
2025-10-30 15:29:34 -0400

    Mask age as a range, display full name

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 
 # Read the JSON file
-with open('people.json', 'r') as file:
+with open('customers.json', 'r') as file:
     people = json.load(file)
 
 # Print first name, age, and phone number for each person
 for person in people:
-    print(f"First Name: {person['first_name']}, Age: {person['age']}, Phone: {person['phone_number']}")
+    print(f"Name: {person['first_name']} {person['last_name']}, Age: {person['age']}, Phone: {person['phone_number']}")
